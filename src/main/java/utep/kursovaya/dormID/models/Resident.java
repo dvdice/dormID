3f3cd98170dc8e683046c3fbe3dc4ddc91622816
dvdice <valitovdamir73@gmail.com>
2024-02-10 11:57:33 +0400

    role+email+pass

--- src/main/java/utep/kursovaya/dormID/models/Resident.java
+++ src/main/java/utep/kursovaya/dormID/models/Resident.java
@@ -5,6 +5,13 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import utep.kursovaya.dormID.models.enums.Role;
+
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "Residents")
@@ -20,4 +27,25 @@ public class Resident {
     private String name;
     @Column(name = "surname")
     private String surname;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "active")
+    private boolean active;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "image_id")
+    private Image avatar;
+    @Column(name = "password", length = 1000)
+    private String password;
+    @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<Role> roles = new HashSet<>();
+    private LocalDateTime dateOfCreated;
+
+    @PrePersist
+    private void init() {
+        dateOfCreated = LocalDateTime.now();
+    }
 }
---
+++ src/main/java/utep/kursovaya/dormID/models/enums/Role.java
@@ -0,0 +1,5 @@
+package utep.kursovaya.dormID.models.enums;
+
+public enum Role {
+    ROLE_USER, ROLE_ADMIN
+}
